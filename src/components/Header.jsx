6db6d3b8a0208fe7692033e77fd4f3ef0237161e
qxan <mehmet.enfa@gmail.com>
2022-10-20 15:58:14 +0300

    style loading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       .get("https://randomuser.me/api/")
       .then((res) => {
         console.log(res);
-        setData(res.data.result[0]);
+        setData(res.data.results[0]);
       })
       .catch((err) => console.log(err));
   };
@@ -22,7 +22,17 @@ const Header = () => {
     <div className="w-full min-h-screen flex flex-col justify-center items-center">
       <div className="bosss">
         {data && (
-          <div className="flex justify-between items-center flex-col w-[30rem] h-[30rem] mx-auto text-center bg-blue-300"></div>
+          <div className="flex justify-between items-center flex-col w-[30rem] h-[30rem] mx-auto text-center bg-blue-400 p-4">
+            <img
+              className="rounded-full w-[9rem]"
+              src={data.picture.medium}
+              alt={data.name.first}
+            />
+            <p>{data.name.title} {data.name.first} {data.name.last}</p>
+            <p>{data.email}</p>
+            <p>{data.phone}</p>
+            <p>{data.location.city}-{data.location.country}</p>
+          </div>
         )}
       </div>
     </div>
